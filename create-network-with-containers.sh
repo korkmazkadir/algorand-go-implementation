7b322ee91451460590c66335d4a2153780b6b77a
Ubuntu <korkmazkadir@gmail.com>
2020-11-22 18:25:33 +0000

    Adds registery addres parameter to the script

--- create-network-with-containers.sh
+++ create-network-with-containers.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+
+read -p "address of the registery: " address_of_registery
 read -p "number of nodes: " number_of_nodes
+
 
 for (( i=1; i<=$number_of_nodes; i++ ))
 do  
    
-    docker run -d --network app-tier --cap-add NET_ADMIN  --mount source=node-data,target=/application/data   algorand:latest -registery="6e0525c7945c:1234"  
+    docker run -d --network app-tier --cap-add NET_ADMIN  --mount source=node-data,target=/application/data   algorand:latest -registery="${address_of_registery}:1234"  
 
     #2> /applicationdata/"$i.log"
 
